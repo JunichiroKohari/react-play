7f4f51ffd7b90896f4ee5cd3ae154f672e18e36c
JunichiroKohari <junichiro@kohari.jp>
2023-04-11 21:40:45 +0900

    css modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { useState, useCallback, useMemo } from 'react'
-import { ChildArea } from './ChildArea';
 import { InlineStyle } from './components/InlineStyle';
+import { CssModules } from './components/CssModules';
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
