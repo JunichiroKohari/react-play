897c772d25825c1477a34b168266f38d85accd89
JunichiroKohari <junichiro@kohari.jp>
2023-04-11 21:18:55 +0900

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import { ChildArea } from './ChildArea';
 
 function App() {
@@ -7,13 +7,14 @@ function App() {
   const [open, setOpen] = useState(false)
   const onChangeText = (e) => setText(e.target.value)
   const onClickOpen = () => setOpen(!open)
+  const onClickClose = useCallback(() => setOpen(false), [])
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -2,13 +2,13 @@ import { memo } from 'react'
 
 const style = {
     width: '100%',
-    height: '100%',
+    height: '200px',
     backgroundColor: 'khaki'
 }
 
 export const ChildArea = memo((props) => {
-    const { open } = props
-    console.log('rendring')
+    const { open, onClickClose } = props
+    console.log('ChildArea is rendered!')
 
     const data = [...Array(2000).keys()]
     data.forEach(() => console.log('...'))
@@ -17,6 +17,7 @@ export const ChildArea = memo((props) => {
             {open ? (
                 <div style={style}>
                     <p>子コンポーネント</p>
+                    <button onClick={onClickClose}>閉じる</button>
                 </div>
             ) : null}
         </>
