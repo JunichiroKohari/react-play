2ea4c23de3c854c01b7ec5187415f20fff836bb3
JunichiroKohari <junichiro@kohari.jp>
2023-04-11 21:47:01 +0900

    Styled jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/CssModules';
+import { StyledJsx } from './components/StyledJsx';
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
