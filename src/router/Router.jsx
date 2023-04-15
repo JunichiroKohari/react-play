e34d3a142381c16e5d4ed7a7465efa667c7b46dc
JunichiroKohari <junichiro@kohari.jp>
2023-04-15 09:45:51 +0900

    loop routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,37 +1,33 @@
 import { Switch, Route } from 'react-router-dom';
-
-import { Home } from './Home';
-import { Page1 } from './Page1';
-import { Page2 } from './Page2';
-import { Page1DetailA } from './Page1DetailA';
-import { Page1DetailB } from './Page1DetailB';
+import { page1Routes } from './Page1Routes';
+import { Home } from '../Home';
+import { Page2 } from '../Page2';
 
 export const Router = () => {
     return (
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route
-            path="/page1"
-            render={({ match: { url } }) => (
-              <Switch>
-                <Route exact path={url}>
-                  <Page1 />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              {page1Routes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  {route.children}
                 </Route>
-                <Route path={`${url}/detailA`}>
-                  <Page1DetailA />
-                </Route>
-                <Route path={`${url}/detailB`}>
-                  <Page1DetailB />
-                </Route>
-                <Page1 />
-              </Switch>
-          )}>
-          </Route>
-          <Route path="/page2">
-            <Page2 />
-          </Route>
+              ))}
+            </Switch>
+        )}>
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
       </Switch>
     )
 }
