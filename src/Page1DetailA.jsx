355e32330ef9319f0579b59534d1960a34c88fa9
JunichiroKohari <junichiro@kohari.jp>
2023-04-15 10:51:03 +0900

    pass state

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,11 @@
+import { useLocation } from "react-router-dom"
+
 export const Page1DetailA = () => {
-    return (
-        <div>
-            <h1>Page1DetailA</h1>
-        </div>
-    )
+  const { state } = useLocation()
+  console.log(state)
+  return (
+    <div>
+        <h1>Page1DetailA</h1>
+    </div>
+  )
 }
