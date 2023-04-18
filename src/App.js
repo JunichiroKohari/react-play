e9a66b534ba0faee20420c0546df8f4577ee1297
JunichiroKohari <junichiro@kohari.jp>
2023-04-18 22:09:55 +0900

    use user card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UserCard';
+
+const user = {
+  name: 'ほげ',
+  image: 'https://source.unsplash.com/eoqnr8ikwFE',
+  email: 'hoge@example.com',
+  phone: '0120-333-906',
+  company: {
+    name: 'テスト株式会社',
+  },
+  website: 'https://google.com'
+}
 
 function App() {
   return (
@@ -12,6 +24,7 @@ function App() {
         <SecondaryButton>検索</SecondaryButton>
         <br />
         <SearchInput />
+        <UserCard user={user} />
       </div>
     </BrowserRouter>
   );
