4ddda07086422f45674e2395f1c83395335b4151
JunichiroKohari <junichiro@kohari.jp>
2023-04-18 22:09:40 +0900

    user card

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,21 @@
+import { Card } from "../../atoms/card/Card"
+import { UserIconWithName } from "../../molecules/user/UserIconWithName"
+
+export const UserCard = (props) => {
+  const { user } = props
+  return (
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <dl className="profile-dl">
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
+        <dt>WEB</dt>
+        <dd>{user.website}</dd>
+      </dl>
+    </Card>
+  )
+}
