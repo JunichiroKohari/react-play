f7687e533e9c663c7b50d4201e1db804da3ae820
JunichiroKohari <junichiro@kohari.jp>
2023-04-21 22:06:20 +0900

    grid user cards

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
 import { DefaultLayout } from './components/templates/DefaultLayout';
-
-const user = {
-  name: 'ほげ',
-  image: 'https://source.unsplash.com/eoqnr8ikwFE',
-  email: 'hoge@example.com',
-  phone: '0120-333-906',
-  company: {
-    name: 'テスト株式会社',
-  },
-  website: 'https://google.com'
-}
+import { Router } from './router/Router';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
+    <Router />
   );
 }
 
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,7 +1,30 @@
+import { SearchInput } from '../molecules/SearchInput'
+import { UserCard } from '../organisms/user/UserCard'
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: 'ほげ' + val,
+    image: 'https://source.unsplash.com/eoqnr8ikwFE',
+    email: 'hoge@example.com',
+    phone: '0120-333-906',
+    company: {
+      name: 'テスト株式会社',
+    },
+    website: 'https://google.com'
+  }
+})
+
 export const Users = () => {
   return (
     <div className="page-container">
       <h2>USERS PAGE</h2>
+      <SearchInput />
+      <div className="user-area">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
     </div>
   )
 }
