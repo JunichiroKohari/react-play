ef5eb2ac1916ba76f4fc29a2e7bc76afd36ba982
JunichiroKohari <junichiro@kohari.jp>
2023-04-24 21:05:57 +0900

    recoil root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Router } from './router/Router';
 import { UserProvider } from './providers/UserProvider';
 import './App.css';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <UserProvider>
-      <Router />
-    </UserProvider>
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
   );
 }
 
