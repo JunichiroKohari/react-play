0c3f65fe46808bf153d633f5f032c663e5db08af
JunichiroKohari <junichiro@kohari.jp>
2023-04-30 08:21:35 +0900

    type FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Router } from './router/Router';
-import { UserProvider } from './providers/UserProvider';
+import { User } from './types/user';
 import './App.css';
 import { RecoilRoot } from 'recoil';
 import { Practice1 } from './practices/Practice1';
+import { Text } from './components/atoms/text/Text';
 import axios from 'axios';
 import { useState } from 'react';
 import { Todo } from './Todo';
 import { TodoType } from './types/todo';
+import { UserProfile } from './UserProfile';
+
+const user: User = {
+  name: 'kohari',
+  hobbies: ['映画', '散歩']
+}
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([])
@@ -17,6 +24,8 @@ function App() {
   }
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>fetch data</button>
       {todos.map(todo => {
         return <Todo
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react'
 import { TodoType } from './types/todo';
 
-export const Todo = (props: Pick<TodoType, "userId" | "title" | "completed">) => {
+export const Todo: FC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props
   const completeMark = completed ? '[完]' : '[未]'
   return (
