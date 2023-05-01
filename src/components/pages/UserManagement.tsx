b27a98a65b7b858d7f0804e661f064a734227e78
JunichiroKohari <junichiro@kohari.jp>
2023-05-02 07:41:46 +0900

    use UserCard in UserManagement

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,20 @@
+import {
+  Wrap,
+  WrapItem
+} from '@chakra-ui/react'
 import { memo, FC } from 'react'
+import { UserCard } from '../organisms/user/UserCard'
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページ</p>
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="Taro"
+          fullName='Taro Yamada'
+        />
+      </WrapItem>
+    </Wrap>
+  )
 })
